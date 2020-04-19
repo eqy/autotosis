6710f13434699adf62ffe089509ff7e66404955a
Eddie Yan <Loser777@gmail.com>
2020-04-19 15:06:23 -0700

    decrease print frequency

--- artosisnet.py
+++ artosisnet.py
@@ -52,7 +52,7 @@ parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
 parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
                     metavar='W', help='weight decay (default: 1e-4)',
                     dest='weight_decay')
-parser.add_argument('-p', '--print-freq', default=10, type=int,
+parser.add_argument('-p', '--print-freq', default=100, type=int,
                     metavar='N', help='print frequency (default: 10)')
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
