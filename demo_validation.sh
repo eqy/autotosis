8e626bcff6985345f1994ee852d5779a71b3090b
Eddie Yan <Loser777@gmail.com>
2020-04-25 00:07:48 -0700

    add another clip

--- demo_validation.sh
+++ demo_validation.sh
@@ -1,2 +1,3 @@
 python3 inference.py -s rawdata/winningtolosing.mp4 -n val_data_winningtolosing.mp4
 python3 inference.py -s rawdata/whatohmy.mp4 -n val_data_whatohmy.mp4
+python3 inference.py -s rawdata/temprun2a3tosis-works-on-fun-damentalstemp5.mp4 -n val_data_example3.mp4
