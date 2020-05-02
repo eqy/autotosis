e7259a900d102ffbb60ed842e5a7c42cadd30c39
Eddie Yan <Loser777@gmail.com>
2020-05-02 00:27:19 -0700

    update

--- demo_validation.sh
+++ demo_validation.sh
@@ -1,3 +1,6 @@
+python3 inference.py -s rawdata/temprun2a3tosis-works-on-fun-damentalstemp5.mp4 -n val_data_example3.mp4
+python3 inference.py -s rawdata/temptesttaking-no-prisoners_highlight5.mp4 -n val_data_example4.mp4
+python3 inference.py -s rawdata/temptest-gsl-tonight_highlight6.mp4 -n val_data_example5.mp4
 python3 inference.py -s rawdata/itssohard.mp4 -n val_data_itssohard.mp4
 python3 inference.py -s rawdata/whatohmy.mp4 -n val_data_whatohmy.mp4
-python3 inference.py -s rawdata/temprun2a3tosis-works-on-fun-damentalstemp5.mp4 -n val_data_example3.mp4
+python3 inference.py -s rawdata/winningtolosing.mp4 -n val_data_winningtolosing.mp4
