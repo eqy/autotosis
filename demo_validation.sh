79f3e2a7db232c9fd673e9adcd9d7119b4ef9f35
Eddie Yan <Loser777@gmail.com>
2020-06-21 17:34:04 -0700

    update

--- demo_validation.sh
+++ demo_validation.sh
@@ -1,6 +1,6 @@
-python3 inference.py -s rawdata/temprun2a3tosis-works-on-fun-damentalstemp5.mp4 -n val_data_example3.mp4 --model-path model_best.pth.tar
-python3 inference.py -s rawdata/temptesttaking-no-prisoners_highlight5.mp4 -n val_data_example4.mp4 --model-path model_best.pth.tar
-python3 inference.py -s rawdata/temptest-gsl-tonight_highlight6.mp4 -n val_data_example5.mp4 --model-path model_best.pth.tar
-python3 inference.py -s rawdata/itssohard.mp4 -n val_data_itssohard.mp4 --model-path model_best.pth.tar
-python3 inference.py -s rawdata/whatohmy.mp4 -n val_data_whatohmy.mp4 --model-path model_best.pth.tar
-python3 inference.py -s rawdata/winningtolosing.mp4 -n val_data_winningtolosing.mp4 --model-path model_best.pth.tar
+python3 inference.py -s rawdata/temprun2a3tosis-works-on-fun-damentalstemp5.mp4 -n val_data_example3.mp4 --model-path resnet18noconcatld5checkpoint.pth.tar --sound
+python3 inference.py -s rawdata/temptesttaking-no-prisoners_highlight5.mp4 -n val_data_example4.mp4 --model-path resnet18noconcatld5checkpoint.pth.tar --sound
+python3 inference.py -s rawdata/temptest-gsl-tonight_highlight6.mp4 -n val_data_example5.mp4 --model-path resnet18noconcatld5checkpoint.pth.tar --sound
+python3 inference.py -s rawdata/itssohard.mp4 -n val_data_itssohard.mp4 --model-path resnet18noconcatld5checkpoint.pth.tar --sound
+python3 inference.py -s rawdata/whatohmy.mp4 -n val_data_whatohmy.mp4 --model-path resnet18noconcatld5checkpoint.pth.tar --sound
+python3 inference.py -s rawdata/winningtolosing.mp4 -n val_data_winningtolosing.mp4 --model-path resnet18noconcatld5checkpoint.pth.tar --sound
